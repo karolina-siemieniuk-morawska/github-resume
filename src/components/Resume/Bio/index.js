bcb9972ca1092e7014b7e3f7c7077f8d82ecf398
karolina-siemieniuk-morawska <karolinajustynska@o2.pl>
2020-04-27 16:23:28 +0200

    added bio

--- src/components/Resume/Bio/index.js
+++ src/components/Resume/Bio/index.js
@@ -1,58 +1,112 @@
-import React from 'react';
+import React from "react";
 import "./style.scss";
 
 const Bio = ({ user }) => {
+  const barWidth = (type) => {
+    return {
+      width: `${type * 2}px`,
+    };
+  };
+
   return (
     <div className="bio">
-      <img className="avatar" src={user.avatar_url} alt="avatar" />
-      {user.name !== null && user.name !== "" && <h2><a href={user.html_url}>{user.name}</a></h2>}
-      {user.email !== null && user.email !== "" && (
-        <h3 className="email info">
-          <i className="far fa-envelope"></i>
-          {user.email}
-        </h3>
-      )}
+      <div className="intro">
+        <img className="avatar" src={user.avatar_url} alt="avatar" />
+        {user.name !== null && user.name !== "" && (
+          <h2>
+            <a href={user.html_url}>{user.name}</a>
+          </h2>
+        )}
 
-      {user.location !== null && user.location !== "" && (
-        <h3 className="location info">
-          <i className="fas fa-map-marker-alt"></i> {user.location}
-        </h3>
-      )}
+        <div className="contact-info">
+          {user.email !== null && user.email !== "" && (
+            <h3 className="email info">
+              <i className="far fa-envelope"></i>
+              {user.email}
+            </h3>
+          )}
 
-      {user.blog !== null && user.blog !== "" && (
-        <h3 className="site info">
-          <i className="fas fa-globe"></i>
-          <a href={user.blog}>{user.blog}</a>
-        </h3>
-      )}
+          {user.location !== null && user.location !== "" && (
+            <h3 className="location info">
+              <i className="fas fa-map-marker-alt"></i> {user.location}
+            </h3>
+          )}
 
-      {user.hireable === true ? (
-        <h3 className="hireable info">
-          <i className="fas fa-check"></i> Looking for job
-        </h3>
-      ) : (
-        <h3 className="hireable info">
-          <i className="fas fa-times"></i> Not looking for job
-        </h3>
-      )}
-            {user.bio !== null && user.bio !== "" && (
-        <h4 className="quote">"{user.bio}"</h4>
-      )}
+          {user.blog !== null && user.blog !== "" && (
+            <h3 className="site info">
+              <i className="fas fa-globe"></i>
+              <a href={user.blog}>{user.blog}</a>
+            </h3>
+          )}
 
-<div className="numberData">
-        <h5>Number of public repositories:</h5>
-        <div className="bar">
-          {/* <div className="repo_bar" style={repoStyle}></div> */}
-          <span>{user.public_repos}</span>
+          {user.hireable === true ? (
+            <h3 className="hireable info">
+              <i className="fas fa-check"></i> Looking for job
+            </h3>
+          ) : (
+            <h3 className="hireable info">
+              <i className="fas fa-times"></i> Not looking for job
+            </h3>
+          )}
+
+          {user.company !== null && user.company !== "" && (
+            <h3 className="company">
+              <i class="fas fa-building"></i> {user.company}
+            </h3>
+          )}
         </div>
-        <h5>Number of followers:</h5>
-        <div className="bar">
-          {/* <div className="fans_bar" style={fansStyle}></div> */}
-          <span>{user.followers}</span>
-        </div>
+
+        {user.bio !== null && user.bio !== "" && (
+          <h4 className="quote">"{user.bio}"</h4>
+        )}
+      </div>
+
+      <div className="number-data">
+        <section>
+          <h5>Repositories:</h5>
+          <div>
+            <div
+              className="repo_bar bar"
+              style={barWidth(user.public_repos)}
+            ></div>
+            <span>{user.public_repos}</span>
+          </div>
+        </section>
+
+        <section>
+          <h5>Gists:</h5>
+          <div>
+            <div
+              className="gists_bar bar"
+              style={barWidth(user.public_gists)}
+            ></div>
+            <span>{user.public_gists}</span>
+          </div>
+        </section>
+
+        <section>
+          <h5>Followers:</h5>
+          <div>
+            <div
+              className="fans_bar bar"
+              style={barWidth(user.followers)}
+            ></div>
+            <span>{user.followers}</span>
+          </div>
+        </section>
+
+        <section>
+          <h5>Following:</h5>
+          <div>
+            <div
+              className="idols_bar bar"
+              style={barWidth(user.following)}
+            ></div>
+            <span>{user.following}</span>
+          </div>
+        </section>
       </div>
     </div>
-
-  )
-}
+  );
+};
 export default Bio;
--- src/components/TopBar/index.js
+++ src/components/TopBar/index.js
@@ -3,32 +3,40 @@ import Logo from "../Blocks/Logo/index";
 import { Searchbar } from "../Blocks/Searchbar/index";
 import { Button } from "react-bootstrap";
 import InfoModal from "../InfoModal/index";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInfo } from "@fortawesome/free-solid-svg-icons";
 import "./style.scss";
 
 export const TopBar = ({ handleInput }) => {
+  const info = <FontAwesomeIcon icon={faInfo} />;
+
   const [modalShow, setModalShow] = useState(true);
 
   // store info about recurring user in local storage
   const pageIsVisited = () => {
     localStorage.setItem("isRecurring", true);
-  }
+  };
 
   useEffect(() => {
     if (localStorage.getItem("isRecurring") === "true") {
       setModalShow(false);
     }
-  }, [])
+  }, []);
 
   return (
     <div className="topbar">
       <Logo />
       <div className="topbar-left">
         <Button variant="primary" onClick={() => setModalShow(true)}>
-          Info</Button>
-        <InfoModal show={modalShow} onHide={() => {
-          setModalShow(false);
-          pageIsVisited();
-        }} />
+          {info}
+        </Button>
+        <InfoModal
+          show={modalShow}
+          onHide={() => {
+            setModalShow(false);
+            pageIsVisited();
+          }}
+        />
         <Searchbar handleInput={handleInput} />
       </div>
     </div>
