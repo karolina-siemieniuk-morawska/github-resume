5b6bcc8478bbf2606402ce6cb25067a8045944dc
karolina-siemieniuk-morawska <karolinajustynska@o2.pl>
2020-05-29 11:53:49 +0200

    better mobile timeline view

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
 import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 import Bio from "./Bio";
 import Timeline from "./Timeline";
 import { fetchRepos, fetchUser } from "../../utils/fetchData";
@@ -11,6 +12,7 @@ export default function Resume({ username }) {
   // FontAwesome icon components
   const ex = <FontAwesomeIcon icon={faTimes} />;
   const up = <FontAwesomeIcon icon={faArrowUp} />;
+  const load = <FontAwesomeIcon icon={faSpinner} />;
 
   const [data, setData] = useState({ user: [], repos: [] });
 
@@ -27,6 +29,12 @@ export default function Resume({ username }) {
     return (
       <div className="resume">
         {up} <span>Enter username</span>
+      </div>
+    );
+  } else if (data.user.message === "Not Found" && username !== "") {
+    return (
+      <div className="resume">
+        {load} <span>Loading...</span>
       </div>
     );
   } else if (data.user.message === "Not Found") {
